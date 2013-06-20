7ac82409e89f72e6b9d939aeb39d034265e21be6
Frank Matheron <frankmatheron@gmail.com>
2013-06-20 15:21:54 +0200

    only add toggles on index page

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 jQuery(document).ready(function($) {
     
+    var $body = $('body.index');
+    
     // hide shorturl form and table by default
-    var $shorturl = $('#new_url,#main_table,#shareboxes,#admin_menu ~ p');
+    var $shorturl = $body.find('#new_url,#main_table,#admin_menu ~ p');
+    var $shareboxes = $body.find('#shareboxes');
     $shorturl.first().before($('<h2>URL Shortening</h2>').click(function() {
-        $shorturl.toggle();
+        var visible = $shorturl.is(':visible');
+        if (visible) {
+            $shorturl.hide();
+            $shareboxes.hide();
+        } else {
+            $shorturl.show();
+            if ($shareboxes.find('input#titlelink').val()) {
+                $shareboxes.show();
+            }
+        }
     }));
     $shorturl.hide();
     
     // allow toggling of forwarding rules
-    var $forwarding = $('#new_forwarding_rule, .forwarding-table-container');
+    var $forwarding = $body.find('#new_forwarding_rule, .forwarding-table-container');
     $forwarding.first().prev().click(function() {
        $forwarding.toggle(); 
     });
