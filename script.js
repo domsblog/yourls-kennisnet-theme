65325763246cccfd306d1c66f67432f5477d5273
Frank Matheron <frankmatheron@gmail.com>
2013-06-21 10:22:37 +0200

    expand forwarding rules by default
    shorturls if forwarding rules are not available

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
     
     var $body = $('body.index');
     
-    // hide shorturl form and table by default
+    // allow toggling for short urls
     var $shorturl = $body.find('#new_url,#main_table,#admin_menu ~ p');
     var $shareboxes = $body.find('#shareboxes');
     $shorturl.first().before($('<h2>URL Shortening</h2>').click(function() {
@@ -12,17 +12,23 @@ jQuery(document).ready(function($) {
             $shareboxes.hide();
         } else {
             $shorturl.show();
+            // we only show the shareboxes if they contain content for a
+            // short url.
             if ($shareboxes.find('input#titlelink').val()) {
                 $shareboxes.show();
             }
         }
     }));
-    $shorturl.hide();
     
     // allow toggling of forwarding rules
     var $forwarding = $body.find('#new_forwarding_rule, .forwarding-table-container');
     $forwarding.first().prev().click(function() {
        $forwarding.toggle(); 
     });
+     
+    // show forwarding rules by default
+    if ($forwarding.length > 0) {    
+        $shorturl.hide();
+    }
     
 });
